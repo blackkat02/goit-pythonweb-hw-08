7ccd82140c54f0fc79d08779e1cef66d9720afe7
Borys <boris02374@gmail.com>
2025-09-09 18:24:01 +0300

    added models

--- main.py
+++ main.py
@@ -8,3 +8,41 @@ app = FastAPI()
 def read_root():
     response = {"message": "Welcome to FastAPI"}
     return response
+
+
+
+# from fastapi import FastAPI
+# import logging
+
+# from fastapi import FastAPI, Depends, HTTPException, status
+# from sqlalchemy import text
+# from sqlalchemy.orm import Session
+
+# from src.database.db import session_manager
+
+# app = FastAPI()
+
+
+# @app.get("/")
+# def read_root(db: Session = Depends(session_manager)):
+#     try:
+#         # Make request
+#         result = db.execute(text("SELECT 1")).fetchone()
+#         if result is None:
+#             raise HTTPException(
+#                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+#                 detail="Database is not configured correctly",
+#             )
+#         return {"message": "Welcome to FastAPI!"}
+#     except Exception as e:
+#         print(e)
+#         raise HTTPException(
+#             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+#             detail="Error connecting to the database",
+#         )
+
+
+# if __name__ == "__main__":
+#     import uvicorn
+
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
--- src/database/models.py
+++ src/database/models.py
@@ -1,50 +1,17 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DATE
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DATE, text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from datetime import date
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
+class Contact(Base):
+    __tablename__ = "Contacts"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(50))
+    last_name: Mapped[str] = mapped_column(String(50))
+    email: Mapped[str] = mapped_column(String(50), unique=True, index=True)
+    phone_number: Mapped[str] = mapped_column(String(20), unique=True)
+    birthday: Mapped[date] = mapped_column(DATE, nullable=False)
+    other_info: Mapped[str] = mapped_column(String(250), nullable=True)
 
-class Student(Base):
-    __tablename__ = "students"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    student_name = Column(String, nullable=False)
-    student_last_name = Column(String, nullable=False)
-    group_id = Column(Integer, ForeignKey("groups.id"))
-    ratings = relationship("Rating", back_populates="student")
-    group = relationship("Group", back_populates="students")
-
-
-class Teacher(Base):
-    __tablename__ = "teachers"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    teacher_name = Column(String, nullable=False)
-    subjects = relationship("Subject", back_populates="teacher")
-
-
-class Group(Base):
-    __tablename__ = "groups"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    group_name = Column(String, nullable=False, unique=True)
-    students = relationship("Student", back_populates="group")
-
-
-class Rating(Base):
-    __tablename__ = "ratings"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    rating = Column(Integer, nullable=False)
-    student_id = Column(Integer, ForeignKey("students.id"))
-    subject_id = Column(Integer, ForeignKey("subjects.id"))
-    date = Column(DATE, default=date.today)
-    student = relationship("Student", back_populates="ratings")
-    subject = relationship("Subject", back_populates="ratings")
-
-
-class Subject(Base):
-    __tablename__ = "subjects"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    subject_name = Column(String, nullable=False, unique=True)
-    teacher_id = Column(Integer, ForeignKey("teachers.id"))
-    teacher = relationship("Teacher", back_populates="subjects")
-    ratings = relationship("Rating", back_populates="subject")
